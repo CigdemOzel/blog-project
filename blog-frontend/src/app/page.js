d6c72845f8005411046a24edd8f67aeb2d29b0ad
CigdemOzel <cigdemozel17@gmail.com>
2025-06-26 13:08:41 +0300

    Update page.js

--- blog-frontend/src/app/page.js
+++ blog-frontend/src/app/page.js
@@ -12,7 +12,7 @@ export default function HomePage() {
     const fetchBlogs = async () => {
       try {
         const res = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/api/blogposts?populate=*&sort=publishDate:desc`
+          `${process.env.NEXT_PUBLIC_API_URL}/api/blogposts?populate=*&sort=createdAt:desc`
         );
         const response = await res.json();
         console.log("API'den gelen veri:", response);
