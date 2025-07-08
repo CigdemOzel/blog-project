3329bc17cb7ac43abe9fdd703e31fce7e6b5d444
CigdemOzel <cigdemozel17@gmail.com>
2025-07-08 20:03:15 +0300

    "Fix NODE_ENV typo to enable production settings"

--- blog/config/middlewares.js
+++ blog/config/middlewares.js
@@ -1,12 +1,20 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://blog-project-bice-iota.vercel.app"],
+      headers: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
