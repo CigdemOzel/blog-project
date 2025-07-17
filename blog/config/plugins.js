b7d9f0b105d250ce91b8db507a04fd01b691aee6
CigdemOzel <cigdemozel17@gmail.com>
2025-07-17 14:47:27 +0300

    Fix: Cloudinary config updated

--- blog/config/plugins.js
+++ blog/config/plugins.js
@@ -1,12 +1,12 @@
-module.exports = ({ env }) => ({
+module.exports = {
   upload: {
     config: {
-      provider: "cloudinary",
+      provider: "@strapi/provider-upload-cloudinary",
       providerOptions: {
-        cloud_name: env("STRAPI_CLOUDINARY_NAME"),
-        api_key: env("STRAPI_CLOUDINARY_KEY"),
-        api_secret: env("STRAPI_CLOUDINARY_SECRET"),
+        cloud_name: process.env.STRAPI_CLOUDINARY_NAME,
+        api_key: process.env.STRAPI_CLOUDINARY_KEY,
+        api_secret: process.env.STRAPI_CLOUDINARY_SECRET,
       },
     },
   },
-});
+};
